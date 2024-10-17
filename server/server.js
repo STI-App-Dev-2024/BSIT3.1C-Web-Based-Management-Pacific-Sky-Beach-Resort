299d38d9a718ca2f42ab59d407c5f839a548fa8e
shannongiron <spgiron26@gmail.com>
2024-10-17 12:24:51 +0800

    CRD functions for rooms

--- server/server.js
+++ server/server.js
@@ -1,29 +1,34 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import colors from 'colors';
-import connectDB from './config/db.js';
-import usersRoutes from './routes/users/usersRoutes.js';
-import tables from './tables/tables.js';
-import conn from './config/db.js';
-import { errorHandler } from './middleware/errorMiddleware.js';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import usersRoutes from "./routes/users/usersRoutes.js";
+import roomsRoutes from "./routes/rooms/roomsRoutes.js";
+import tables from "./tables/tables.js";
+import conn from "./config/db.js";
+import { errorHandler } from "./middleware/errorMiddleware.js";
 
 const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const corsOptions = {
-  origin: '*',
+  origin: "*",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions)); // Use this after the variable declaration
 app.use(express.json());
-app.use(express.urlencoded({ limit: '100mb', extended: true, parameterLimit: 50000 }));
+app.use(
+  express.urlencoded({ limit: "100mb", extended: true, parameterLimit: 50000 })
+);
 
 // Routes
 app.use('/users', usersRoutes);
+
+app.use('/rooms', roomsRoutes);
 
 app.get(`/`, (req, res) => {
   res.send(`Server is running on Port ${PORT}...`);
@@ -35,7 +40,10 @@ app.use(errorHandler);
 const startServer = async () => {
   try {
     app.listen(PORT, () => {
-      console.log(`API is running in ${process.env.NODE_ENV} mode on port ${PORT}`.bold.blue);
+      console.log(
+        `API is running in ${process.env.NODE_ENV} mode on port ${PORT}`.bold
+          .blue
+      );
       connectDB(); // Connect to the database after the server starts
     });
   } catch (error) {
