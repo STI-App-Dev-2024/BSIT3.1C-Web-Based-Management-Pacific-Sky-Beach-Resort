46e704e2c016f89383a373729d6f8296a6101619
Darryle Miles Bacay <dmilesbacay@gmail.com>
2024-10-20 10:06:34 +0000

    Merged PR 27: Maintenance
    feat: some changes

--- client/src/layout/Wrapper/index.jsx
+++ client/src/layout/Wrapper/index.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Navbar from 'layout/Wrapper/Navbar';
 import Footer from './Footer';
 import { Box } from '@mui/material';
+import ScrollTop from 'components/ScrollTop';
 
 const PageWrapper = ({ children }) => {
   return (
     <React.Fragment>
+      <ScrollTop />
       <Navbar />
       <Box sx={{ minHeight: '100vh', my: 2 }}>{children}</Box>
       <Footer />
--- client/src/routes/AbsoluteRoutes.jsx
+++ client/src/routes/AbsoluteRoutes.jsx
@@ -10,6 +10,8 @@ import PageWrapper from 'layout/Wrapper';
 const Home = Loadable(lazy(() => import('pages/home/index')));
 const AboutUs = Loadable(lazy(() => import('pages/about-us/index')));
 const ContactUs = Loadable(lazy(() => import('pages/contact-us/index')));
+const NotFoundPage = Loadable(lazy(() => import('pages/portal/maintenance/404')));
+
 // ==============================|| AUTH ROUTING ||============================== //
 
 const AbsoluteRoutes = {
@@ -19,18 +21,20 @@ const AbsoluteRoutes = {
       path: '/',
       element: <PageWrapper children={<Home />} />
     },
-
     {
       path: '/about-us',
       element: <PageWrapper children={<AboutUs />} />
     },
-
     {
       path: '/contact-us',
       element: <PageWrapper children={<ContactUs />} />
     },
+    {
+      path: '*',
+      element: <NotFoundPage />
+    }
   ]
-  
+
 };
 
 export default AbsoluteRoutes
