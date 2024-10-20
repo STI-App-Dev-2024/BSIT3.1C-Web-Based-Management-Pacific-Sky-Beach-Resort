467a5f518f4086824a0ce80d77b2e07588a3a1ff
Jhon Ace Sasutona <jhonacesasutona718@gmail.com>
2024-10-20 08:14:20 +0000

    Merged PR 25: Create Footer
    - feat: create footer
    - wip: delete code in contact us
    - feat: dynamic year in copy right
    
    Related work items: #43

--- client/src/layout/Wrapper/Footer.jsx
+++ client/src/layout/Wrapper/Footer.jsx
@@ -1,9 +1,102 @@
-import React from 'react'
+import React from 'react';
+import { useTheme } from '@mui/material/styles';
+import { Box, Container, Divider, Stack, Typography } from '@mui/material';
+
 
 const Footer = () => {
+  const theme = useTheme();
+  const date = new Date();
+  const currentYear = date.getFullYear();
   return (
-    <div>Footer</div>
-  )
-}
+    <React.Fragment>
+      <Box
+        sx={{
+          backgroundColor: theme.palette.primary.dark,
+          padding: 2,
+          mt: 2
+        }}
+      >
+        <Container>
+          <Stack spacing={5} direction="row" justifyContent="center">
+            <Box>
+              <Typography marginBlock="1em .5em" variant="h5" color="white">
+                Company Info
+              </Typography>
+              <Typography variant="body2" color="white">
+                About Us
+                <br />
+                Our Mission
+                <br />
+                Our Vission
+              </Typography>
+            </Box>
 
-export default Footer
+            <Box>
+              <Typography marginBlock="1em .5em" variant="h5" color="white">
+                Solutions
+              </Typography>
+              <Typography variant="body2" color="white">
+                For Families
+                <br />
+                For Individuals
+                <br />
+                For Agents
+              </Typography>
+            </Box>
+
+            <Box>
+              <Typography marginBlock="1em .5em" variant="h5" color="white">
+                Features
+              </Typography>
+              <Typography variant="body2" color="white">
+                CFS Edge
+                <br />
+                CFS Advantage
+              </Typography>
+            </Box>
+
+            <Box>
+              <Typography marginBlock="1em .5em" variant="h5" color="white">
+                Resources
+              </Typography>
+              <Typography variant="body2" color="white">
+                Free Agent Training
+                <br />
+                Blog
+              </Typography>
+            </Box>
+
+            <Box>
+              <Typography marginBlock="1em .5em" variant="h5" color="white">
+                Get In Touch
+              </Typography>
+              <Typography variant="body2" color="white">
+                +1(702) 900-5666
+                <br />
+                Las Vegas, NV
+              </Typography>
+            </Box>
+          </Stack>
+        </Container>
+        <Divider
+          sx={{
+            mx: 30,
+            marginTop: 10
+          }}
+        />
+        <Typography marginInline="5em" paddingBlock="3em 1em" variant="body1" color="white">
+          Copyright  {currentYear} | Pacific Sky Beach
+        </Typography>
+      </Box>
+
+      <Box
+        sx={{
+          backgroundColor: theme.palette.error.main,
+          padding: 2
+        }}
+      ></Box>
+    </React.Fragment>
+  );
+};
+
+export default Footer;
