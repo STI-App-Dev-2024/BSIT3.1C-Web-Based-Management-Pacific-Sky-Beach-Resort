dba061f7f5402ce73025b653396abe57c68df119
Darryle Miles Bacay <dmilesbacay@gmail.com>
2024-11-19 12:32:25 +0000

    Merged PR 73: docs: update tables directories
    docs: update tables directories
    
    Cherry picked from !72

--- server/tables/tables.js
+++ server/tables/tables.js
@@ -1,8 +1,8 @@
-import usersTable from './usersTable.js';
-import roomsTable from './roomsTable.js';
-import roomsPictures from './roomsPicturesTable.js';
-import roomBedTable from './roomBedTable.js';
-import roomBathroomTable from './roomBathroomTable.js';
+import usersTable from './users/usersTable.js';
+import roomsTable from './rooms/roomsTable.js';
+import roomsPictures from './rooms/roomsPicturesTable.js';
+import roomBedTable from './rooms/roomBedTable.js';
+import roomBathroomTable from './rooms/roomBathroomTable.js';
 
 const tables = async (dbConnection) => {
   const queries = [
