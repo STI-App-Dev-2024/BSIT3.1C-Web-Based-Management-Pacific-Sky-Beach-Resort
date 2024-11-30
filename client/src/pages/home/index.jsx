dc599d77d95ab97e34c3ebc24acf90a06b694e63
Jhon Ace Sasutona <jhonacesasutona718@gmail.com>
2024-11-30 11:36:43 +0000

    Merged PR 91: Enhance Home Page And Add Pictures in Assets
    - fix: Enchance Home Page
    
    ![image.png](https://dev.azure.com/Darryle-Environment/11d9f155-cc86-4753-8453-2e4a6294aa5d/_apis/git/repositories/c4678471-cdfe-413f-9190-370c5d712c2a/pullRequests/91/attachments/image.png)

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,22 +1,59 @@
 import React from 'react'
-import Banner from 'components/Banner'
-import { Box } from '@mui/material'
+import { Box, Stack, Typography } from '@mui/material'
+import Carousel from 'components/Carousel'
 
-const index = () => {
+import pic1 from 'src/assets/images/upload/pic1.jpg'
+import pic2 from 'src/assets/images/upload/pic2.jpg'
+import pic3 from 'src/assets/images/upload/pic3.jpg'
+import pic4 from 'src/assets/images/upload/pic4.jpg'
+
+const Home = () => {
   return (
+    <Stack
+      sx={{
+        height: '100vh',
+        width: '100%',
+        margin: 0,
+        padding: 0,
+        position: 'relative',
 
+      }}
+    >
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100vh',
+          zIndex: -1,
+        }}
+      >
+        <Carousel
+          items={[
+            pic1, pic2, pic3, pic4
+          ]}
+        />
+      </Box>
+      <Box
+        sx={{
+          position: 'absolute',
+          top: '40%',
+          left: '10%',
+          zIndex: 1,
+          maxWidth: '300px',
+        }}
+      >
+        <Typography mb={2} variant="h2" color="#fff">
+          Pacific Sky Beach
+        </Typography>
+        <Typography variant="body1" color="#fff">
+          “Pacific” means “family” in the ancient Sanskrit language, a return to meaningful family traditions is at the core of everything at Pacific Sky Beach.
+        </Typography>
+      </Box>
 
-    <Box>
-      <Banner
-        image="https://cdn.wallpapersafari.com/77/3/PdvkpF.jpg"
-        title="DISCOVER THE PACIFIC SKY BEACH"
-        subtitle="BREATH TAKING SCENIC VIEWS"
-        description="You know you've found a treasure trove when family encounters with nature are a tradition,
-                     and you've given them wonderful memories to last a lifetem. "
-      />
-    </Box>
-
+    </Stack>
   )
 }
 
-export default index
+export default Home
